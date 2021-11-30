28d6aecea72a32c4beeacbe07408f06399476b2e
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-29 23:27:24 -0700

    Add psuedocode for turn test

---
+++ test/Turn-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  //should be a function
+
+  //should be an instance of Turn
+
+  //should store guess
+
+  //should store card in play - card should be instance of card
+
+  //should return the guess - returnGuess
+
+  //should return the card in play - returnCard
+
+  //should evaluate guess - evaluateGuess
+
+  //should give feedback about guess - giveFeedback
+})
