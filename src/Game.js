7b96ed85955eb2a96b44f1784519f34ecd426b0d
Ali Roemhildt <aliroemhildt@gmail.com>
2021-12-02 02:09:07 -0700

    Make game class almost fully functional

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,27 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor(data) {
+    this.data = data;
+    this.start();
+
+  }
+
+  start() {
+    let cards = this.data.map(element => {
+      return new Card(element.id, element.question, element.answers, element.correctAnswer);
+    })
+    this.deck = new Deck(cards);
+    this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.round);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+  var data = [{
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    }, {
+      "id": 2,
+      "question": "What is a comma-separated list of related values?",
+      "answers": ["array", "object", "function"],
+      "correctAnswer": "array"
+    }, {
+      "id": 3,
+      "question": "What type of prototype method directly modifies the existing array?",
+      "answers": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "mutator method"
+    }];
+  var game = new Game(data);
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  })
+
+  it('should be an instancs of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  })
+
+  it('should keep track of current round', function() {
+    expect(game.currentRound).to.be.an.instanceof(Round);
+  })
+
+  //need to test start method
+
+})
